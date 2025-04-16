1becd17d54b002a13ee78372234e822be7ff46b4
Yuehan He <mperform@umich.edu>
2025-04-15 20:59:54 -0400

    efficientnet Co-authored-by: zhouxq612 <zhouxq612@users.noreply.github.com>

---
+++ EfficientNet_Baseline/dataset.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import h5py
+import torch
+import cv2
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import os
+from torch.utils.data import Dataset
+from torchvision import transforms, models
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from tqdm import tqdm
+
+class HDF5Dataset(Dataset):
+    def __init__(self, images, labels, augment=False):
+        self.images = images
+        self.labels = labels
+        self.augment = augment
+        self.transform = transforms.Compose([
+            transforms.ToPILImage(),
+            # transforms.Resize((128, 128)),
+            transforms.Resize((300, 300)),
+            transforms.RandomHorizontalFlip() if augment else transforms.Lambda(lambda x: x),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5]*3, [0.5]*3)
+        ])
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image = self.images[idx]
+        label = self.labels[idx]
+        image = self.transform(image)
+        return image, torch.tensor(label, dtype=torch.float32)
