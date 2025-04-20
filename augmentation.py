0a8d1e0bd860686df5bf5743ef316606c90540a1
Yuehan He <mperform@umich.edu>
2025-04-20 19:27:04 -0400

    augmentation

--- augmentation.py
+++ augmentation.py
@@ -77,4 +77,50 @@ augmented_metadata_df.to_csv(augmented_metadata_path, index=False)
 print(f"Augmented metadata saved to {augmented_metadata_path}")
 
     
+# augment the isic_image.hdf5 as well
+isic_hdf5_path = 'isic_image.hdf5'
+isic_metadata_path = 'isic_metadata.csv'
+isic_metadata = pd.read_csv(isic_metadata_path,low_memory=False)
+isic_hdf5 = h5py.File(isic_hdf5_path, 'r')
+isic_images = []
+isic_images_idx = []
+for i in tqdm(range(len(isic_metadata))):
+    if isic_metadata.iloc[i]['benign_malignant'] == 'malignant': # skip non-target images
+        image_id = isic_metadata.iloc[i]['isic_id']
+        image = isic_hdf5[image_id][()]
+        image = np.frombuffer(image, dtype=np.uint8)
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        image = cv2.resize(image, (128, 128))
+        image = image / 255
+        isic_images.append(image)
+        isic_images_idx.append(i)
+
+print(f'Found {len(isic_images)} malignant images in ISIC dataset')
+# augment the images and save to hdf5 and also save the metadata
+hdf5_file = h5py.File('isic_augmented_data.hdf5', "w")
+augmented_metadata = []
+
+for idx, img in tqdm(zip(isic_images_idx, isic_images), total=len(isic_images), desc="Augmenting ISIC data"):
+    # orig_isic_id = train_metadata.iloc[idx]['isic_id']
+    # target = train_metadata.iloc[idx]['target']
+    img_uint8 = (img * 255).astype("uint8")
     
+    for j in range(n_augmentations):
+        aug_tensor = augmentation(img_uint8)  # Tensor CxHxW
+        aug_img_np = (aug_tensor.permute(1, 2, 0).numpy() * 255).astype(np.uint8)
+
+        new_id = f"{isic_metadata.iloc[idx]['isic_id']}_aug{j}"
+        success, encoded_img = cv2.imencode(".png", aug_img_np)
+        if not success:
+            print(f"[Warning] Failed to encode {new_id}")
+            continue
+
+        # Store encoded image as bytes
+        hdf5_file.create_dataset(new_id, data=encoded_img)
+        temp = isic_metadata.iloc[idx]
+        temp['isic_id'] = new_id
+        augmented_metadata.append(temp.to_dict())
+hdf5_file.close()
+augmented_metadata_df = pd.DataFrame(augmented_metadata)
+augmented_metadata_df.to_csv('isic_augmented_metadata.csv', index=False)
+print(f"Augmented metadata saved to isic_augmented_metadata.csv")
